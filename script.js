3c8a37fdfdccd0b219486c4813167c852c860c1c
Arif Hussain <arifh9821@gmail.com>
2024-09-26 14:26:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
     //Write your code here
-
-
+const strongs = document.querySelectorAll('strong')
+strongs.forEach(ele=>{
+	ele.classList.add('highlighted');
+});
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+ const strongTags = document.querySelectorAll('strong');
+  strongTags.forEach(tag => tag.classList.remove('highlighted'));
 }
